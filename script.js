f34d27dd093f9d808389a8baa29f942e8d3a43a3
omassey1 <65238159+omassey1@users.noreply.github.com>
2022-12-05 14:21:36 -0500

    questions

--- script.js
+++ script.js
@@ -2,3 +2,31 @@ var timerDownalod;
 var timeleft = 60;
 var playerrScore = 0;
 var playerInitials;
+
+var questions = [
+    {
+      question: "Inside which HTML element do we put the JavaScript?",
+      answers: ["<javascript>", "<js>", "<script>", "<scripting>"],
+      correctAnswer: "<script>",
+    },
+    {
+      question: "How does a for loop start?",
+      answers: ["for (i = 0; i <= 5)", "for (i = 0; i <= 5; i++)", "for i = 1 to 5", "for (i <= 5; i++)"],
+      correctAnswer: "for (i = 0; i <= 5; i++)",
+    },
+    {
+      question: "How can you add a comment in a JavaScript?",
+      answers: ["//This is a comment ", "'This is a comment", "<!--This is a comment-->", "#This is a comment"],
+      correctAnswer: "//This is a comment",
+    },
+    {
+      question: "In JavaScript, which of the following is a logical operator?",
+      answers: ["|", "%", "/", "&&"],
+      correctAnswer: "&&",
+    },
+    {
+      question: "A named element in a JavaScript program that is used to store and retrieve data is a _____.",
+      answers: ["Method", "assignment operator", "Variable", "string"],
+      correctAnswer: "Variable",
+    },
+  ];
